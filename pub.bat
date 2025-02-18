f7cd674d49b7bb616ff6daac8748bc41e2776588
Younes M'RABTI <younesmrabti50@gmail.com>
2025-02-18 13:57:47 +0100

    new pre-release version

--- pub.bat
+++ pub.bat
@@ -1,4 +1,53 @@
-git tag v%2
+@echo off
+setlocal enabledelayedexpansion
+
+:: Initialize variables
+set "commitMessage="
+set "tagName="
+set "remoteName="
+
+:: Parse named parameters
+:parse
+if "%~1"=="" goto validate
+
+if "%~1"=="-m" (
+    set "commitMessage=%~2"
+    shift
+)
+if "%~1"=="-t" (
+    set "tagName=%~2"
+    shift
+)
+if "%~1"=="-r" (
+    set "remoteName=%~2"
+    shift
+)
+shift
+goto parse
+
+:: Validate required parameters
+:validate
+if "%commitMessage%"=="" (
+    echo Error: Missing commit message. Use -m "message"
+    exit /b 1
+)
+
+if "%tagName%"=="" (
+    echo Error: Missing tag name. Use -t "tag_name"
+    exit /b 1
+)
+
+if "%remoteName%"=="" (
+    echo Error: Missing remote name. Use -r "remote"
+    exit /b 1
+)
+
+:: Commit, tag, and push
+echo "%commitMessage%"
 git add .
-git commit -m "%1"
-git push -u origin main
+git commit -m "%commitMessage%"
+git tag "%tagName%"
+git push --tags
+git push -u "%remoteName%"
+
+echo Done!
